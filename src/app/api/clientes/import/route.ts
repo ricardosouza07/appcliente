5fffeeba2116a571d7339d906846fd19934a886b
Ricardo Souza <ricardosouza07@github.com>
2026-04-16 22:08:11 -0300

    fix: compatibility adjust for vercel postgres and static compilation

--- src/app/api/clientes/import/route.ts
+++ src/app/api/clientes/import/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import * as xlsx from 'xlsx';
 
+export const dynamic = 'force-dynamic';
+
 const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
